3f2b9f4fa016173dc0cbf4c844e3afc6a5f6d31b
testusuke <silverbro007@icloud.com>
2022-12-30 10:30:51 +0900

    feat: redis and update build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-
 # shutdown containers
 docker compose down
 
 # update
-if [ $# -ne 1 ]; then
-  git submodule foreach git checkout $1
-  git submodule foreach git fetch
-  git submodule foreach git pull origin $1
-else
-  git submodule foreach git checkout main
-  git submodule foreach git fetch
-  git submodule foreach git pull origin main
-fi
+git submodule foreach git checkout main
+git submodule foreach git fetch
+git submodule foreach git pull origin main
 
 # start
 docker compose up -d
